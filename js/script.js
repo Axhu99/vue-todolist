1ef606c127da96a65f4902148d3ccf41537b6277
Axhu99 <xhuliodreni@icloud.com>
2024-01-23 18:50:04 +0100

    add milestone 3

--- js/script.js
+++ js/script.js
@@ -8,8 +8,8 @@ const app = createApp({
             { id: 2, done: true, text: 'Lavare i capelli' },
             { id: 3, done: false, text: 'Comprare una marca da bollo' },
             { id: 4, done: false, text: 'Aggiornare il PC' }
-        ]
-
+        ],
+        newTodo: ''
     }),
 
     methods: {
@@ -19,8 +19,19 @@ const app = createApp({
                 else return false;
             })
             this.todos = newTodos
+        },
+
+        addTodo(){
+            const newTodo = {
+                id: 7,
+                done: false,
+                text: this.newTodo
+            }
+
+            return this.todos.push(newTodo) 
         }
     }
+
 });
 
 app.mount('#root');
